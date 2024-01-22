f88ba3f9128456e9c2ee4e01cab398d39a0b43b6
Pavan19082001 <63573888+Pavan19082001@users.noreply.github.com>
2024-01-22 18:03:53 +0530

    Delete src/com/pavan directory

--- src/com/pavan/SpecialCharsInString.java
+++
@@ -1,8 +0,0 @@
-package com.pavan;
-
-public class SpecialCharsInString {
-	public static void main(String[] args) {
-		String s = "java@#25%off";
-		System.out.println(s.replaceAll("[^A-Za-z0-9]", ""));
-	}
-}
